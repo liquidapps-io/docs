fbc613b9acb9b6534699bc3c8fa7a87a76e23732
Tal Muskal <tal@liquidapps.io>
2019-04-07 04:43:30 +0000

    sync

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'LiquidApps'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['recommonmark','sphinx.ext.todo']
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -39,13 +39,16 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 master_doc = 'index'
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'neo_rtd_theme'
+import sphinx_theme
+html_theme_path = [sphinx_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
